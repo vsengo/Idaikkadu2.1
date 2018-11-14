d75ee1cdbbd0cd21ce7c6fcf6631d50f73cec033
sengo <sengovel@yahoo.com>
2018-11-13 20:32:56 -0500

    Working version of photo uploads and updating album

--- photos/urls.py
+++ photos/urls.py
@@ -5,8 +5,9 @@ app_name='photos'
 
 urlpatterns = [
     url(r'^get_description/$', views.get_description, name='get_description'),
-    url(r'^photo_form$', views.PhotoView.as_view(), name='photo_form'),
-    url(r'^save_photos$', views.PhotoView.as_view(), name='photo_form'),
-    url(r'^save/', views.AlbumView.as_view(), name='photo_form_save'),
+    url(r'^add_to_album/$', views.add_to_album, name='add_to_album'),
+    url(r'^photo_form$', views.load_photoform, name='load_photo_form'),
+    url(r'^save/$', views.AlbumView.as_view(), name='album_form_save'),
     url(r'^close_photos$', views.AlbumView.as_view(), name='close_photos'),
+    url(r'^progress-bar-upload/$', views.ProgressBarUploadView.as_view(), name='progress_bar_upload'),
 ]
--- photos/views.py
+++ photos/views.py
@@ -1,7 +1,6 @@
 import time
-
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect
-from django.http import JsonResponse
 from django.http import HttpResponse
 from django.views import View
 import logging
@@ -15,15 +14,47 @@ from .models import Album
 logger = logging.getLogger(__name__)
 
 
-class PhotoView(View):
+def load_photoform(request):
+    photos_list = Photo.objects.filter(album_id__isnull=True)
+    return render(request, 'photos/progress_bar_upload/photoupload.html', {'photos': photos_list})
+
+class AlbumView(View):
     def get(self, request):
-        photos_list = Photo.objects.all()
-        return render(self.request, 'photos/progress_bar_upload/photoupload.html',{'photos':photos_list})
+        albumform = AlbumForm()
+        return render(self.request, 'photos/progress_bar_upload/createalbum.html', {'albumform':albumform})
+
+    def post(self,request):
+        photos_list = Photo.objects.filter(album_id__isnull=True)
+        if "cancel" in request.POST:
+            return render(request, 'photos/progress_bar_upload/photoupload.html', {'photos': photos_list})
+        else:
+            albumform = AlbumForm(request.POST)
+            if albumform.is_valid():
+                album=albumform.save()
+                count = 0
+                for photo in photos_list:
+                    photo.album = album
+                    photo.save()
+                    count +=1
+                return HttpResponse("Alnum is saved with " +str(count) + " Photos")
+
+def get_description(request):
+        albumform = AlbumForm()
+        return render(request, 'photos/progress_bar_upload/createalbum.html', {'albumform': albumform})
+
+
+def add_to_album(request):
+    albumform = AlbumForm()
+    return render(request, 'photos/progress_bar_upload/createalbum.html', {'albumform': albumform})
+
+class ProgressBarUploadView(View):
+    def get(self, request):
+        photos_list = Photo.objects.filter(album_id__isnull=True)
+        return render(self.request, 'photos/progress_bar_upload/createalbum.html', {'photos': photos_list})
 
     def post(self, request):
         time.sleep(1)  # You don't need this line. This is just to delay the process so you can see the progress bar testing locally.
-
-        form = PhotoForm(request.POST, request.FILES)
+        form = PhotoForm(self.request.POST, self.request.FILES)
         if form.is_valid():
             photo = form.save()
             data = {'is_valid': True, 'name': photo.file.name, 'url': photo.file.url}
@@ -31,18 +62,8 @@ class PhotoView(View):
             data = {'is_valid': False}
         return JsonResponse(data)
 
-class AlbumView(View):
-    def get(self, request):
-        albumform = AlbumForm()
-        return render(request, 'photos/progress_bar_upload/index.html', {'albumform':albumform})
-
-    def post(self,request):
-        albumform = AlbumForm(request.POST)
-        if albumform.is_valid():
-            album=albumform.save()
-
-        return render(request, 'photos/progress_bar_upload/index.html', {'albumform': albumform})
-
-def get_description(request):
-        albumform = AlbumForm()
-        return render(request, 'photos/progress_bar_upload/index.html', {'albumform': albumform})
+def clear_database(request):
+        for photo in Photo.objects.filter(album_id__isnull=True):
+            photo.file.delete()
+            photo.delete()
+        return redirect(request.POST.get('next'))
