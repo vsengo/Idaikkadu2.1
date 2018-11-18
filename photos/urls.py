7e422bf6f7cf1c3329d6179b1c693555ff494055
sengo <sengovel@yahoo.com>
2018-11-18 09:32:04 -0500

    photo upload and add to album working version

--- photos/urls.py
+++ photos/urls.py
@@ -6,6 +6,7 @@ app_name='photos'
 urlpatterns = [
     url(r'^get_description/$', views.get_description, name='get_description'),
     url(r'^add_to_album/$', views.add_to_album, name='add_to_album'),
+    url(r'^choose_album/$', views.choose_album, name='choose_album'),
     url(r'^photo_form$', views.load_photoform, name='load_photo_form'),
     url(r'^save/$', views.AlbumView.as_view(), name='album_form_save'),
     url(r'^close_photos$', views.AlbumView.as_view(), name='close_photos'),
--- photos/views.py
+++ photos/views.py
@@ -38,12 +38,27 @@ class AlbumView(View):
                     count +=1
                 return HttpResponse("Alnum is saved with " +str(count) + " Photos")
 
-def get_description(request):
-        albumform = AlbumForm()
-        return render(request, 'photos/progress_bar_upload/createalbum.html', {'albumform': albumform})
-
+def choose_album(request):
+    album_list = Album.objects.filter()
+    return render(request, 'photos/progress_bar_upload/choosealbum.html', {'albums': album_list})
 
 def add_to_album(request):
+    albumid = request.POST.get("AlbumID","empty")
+    photos_list = Photo.objects.filter(album_id__isnull=True)
+
+    albumList = Album.objects.filter(id=albumid)
+    if len(albumList) > 0:
+        album=albumList[0]
+        count =0
+        for photo in photos_list:
+            photo.album = album
+            photo.save()
+            count += 1
+        return HttpResponse(str(count)+" Photos saved to Album "+ album.title)
+    else:
+        return HttpResponse("Could not find Album in the Database")
+
+def get_description(request):
     albumform = AlbumForm()
     return render(request, 'photos/progress_bar_upload/createalbum.html', {'albumform': albumform})
 
@@ -53,7 +68,7 @@ class ProgressBarUploadView(View):
         return render(self.request, 'photos/progress_bar_upload/createalbum.html', {'photos': photos_list})
 
     def post(self, request):
-        time.sleep(1)  # You don't need this line. This is just to delay the process so you can see the progress bar testing locally.
+        time.sleep(100)
         form = PhotoForm(self.request.POST, self.request.FILES)
         if form.is_valid():
             photo = form.save()
