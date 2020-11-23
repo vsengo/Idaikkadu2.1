56715d0cec43ff8545363ea28be327ec1012bdba
Sengo <vsengo@gmail.com>
2020-11-22 19:40:34 -0500

    Photo and Album form working. Can upload and view photos. Loading to album and photos table. removed update_album in urls.py

--- photos/urls.py
+++ photos/urls.py
@@ -6,6 +6,5 @@ app_name = 'photos'
 
 urlpatterns = [
     url(r'^add-album', views.AddAlbum, name='add-album'),
-    url(r'^update-album', views.UpdateAlbum, name='update-album'),
     url(r'^view-photos', views.PhotoList.as_view(), name='view-photos')
 ]
