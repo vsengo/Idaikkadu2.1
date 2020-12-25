efc9b363f8da53f9e8ed9c47b5f953c514883f08
Sengo <vsengo@gmail.com>
2020-12-24 20:28:55 -0500

    forgot/resetting password completed. Functionality tested good.

--- install_aws.bash
+++ install_aws.bash
@@ -1,5 +1,6 @@
 #!/bin/bash
-pkg=Idaikkadu.tar.gz
+pkg=Idaikkadu.tar
+
 if [ $# -ge 1 ] ; then
 	echo "install <pkg>"
 	pkg=$1
@@ -8,5 +9,12 @@ fi
 if [ $# -gt 1 ]; then
 	dest=$2
 fi
+
+echo "Packaging $pkge"
+rm $pkg
+tar -cvf $pkg  accounts idaikkadu media news photos web db.sqlite3
+gzip $pkg 
+
 echo "Installing to AWS instance"
-scp -i /Users/veluppillaisengo/Documents/Keys/LightsailDefaultKey-us-east-1.pem  $pkg bitnami@54.163.186.96:~/stack/projects/idaikkadu/$dest
+scp -i /Users/veluppillaisengo/Documents/Keys/LightsailDefaultKey-us-east-1.pem  ${pkg}.gz bitnami@54.163.186.96:~/stack/projects/idaikkadu/$dest
+./ssh_aws.bash
